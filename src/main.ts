0a21db495e61b78f323ff553d89ba288a1e94a83
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-03-22 08:44:43 -0300

    Firebase config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { corsConfig } from './cors.config';
-import { initializeFirebaseApp } from 'firebase.config';
+import { initializeApp } from 'firebase/app';
 
-initializeFirebaseApp();
+// import { corsConfig } from './cors.config';
+// import { initializeFirebaseApp } from 'firebase.config';
+// initializeFirebaseApp();
+
+const serviceAccount = {
+  apiKey: "AIzaSyBhSgJt8qA5yNNkAvMHXQfgUsPXhB-B4lo",
+  authDomain: "reaproducoes-31713.firebaseapp.com",
+  projectId: "reaproducoes-31713",
+  storageBucket: "reaproducoes-31713.appspot.com",
+  messagingSenderId: "1030329546371",
+  appId: "1:1030329546371:web:835ec1763a2d89e7cb8db6",
+  measurementId: "G-FE7WL2BFKL"
+};
+
+initializeApp(serviceAccount);
 
 async function bootstrap() {
 
