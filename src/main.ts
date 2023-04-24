a83dc649981a8b624e709b678d95c3b64d4f84de
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-04-24 18:41:02 -0300

    same changes

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,8 @@ async function bootstrap() {
     .addTag('app')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
 
   SwaggerModule.setup('api', app, document, {
-    // customSiteTitle: 'Backend Generator',
-    // customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui-standalone-preset.min.js',
