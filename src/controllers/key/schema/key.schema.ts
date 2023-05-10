f4db7f69c928a74cfd494df758d834123a071f8a
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-05-09 23:10:16 -0300

    Default date

--- src/controllers/key/schema/key.schema.ts
+++ src/controllers/key/schema/key.schema.ts
@@ -4,14 +4,7 @@ import * as mongoose from 'mongoose';
 export const KeySchema = new mongoose.Schema({
     album: String,
     value: String,
-    expirationDate: {
-        type: Date,
-        default: function () {
-            const expirationDate = new Date();
-            expirationDate.setDate(expirationDate.getDate() + 180);
-            return expirationDate;
-        }
-    },
+    expirationDate: Date,
     created_at: {
         type: Date,
         default: Date.now,
