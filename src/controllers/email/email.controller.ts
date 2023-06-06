6949ada465677ec9c6c398e7cb27387c7502097d
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-06-06 16:07:54 -0300

    Post Email

--- src/controllers/email/email.controller.ts
+++ src/controllers/email/email.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { MailerService } from '@nestjs-modules/mailer'
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 @Controller('email')
 export class EmailController {
     constructor(private mailService: MailerService) { }
@@ -14,17 +14,20 @@ export class EmailController {
         });
     }
 
-    @Get('app/admin')
-    async sendAdminEmail(
-        @Query('toEmail') toEmail,
-        @Query('type') type,
-        @Query('course') course,
-        @Query('classe') classe,
-        @Query('nome') nome,
-        @Query('imageUrl') imageUrl,
-    ) {
+    @Post('app/admin')
+    async sendToAdmin(@Body() body: any) {
+        const {
+            toEmail,
+            type,
+            course,
+            classe,
+            nome,
+            imageUrl
+        } = body;
+
         const titleContent = `${type} ${course}`;
         const htmlContent = `<h2>Imagem escolhida por ${nome}</h2>.<br> <h3>Curso:${course}</h3><br> <h3>Turma:${classe}</h3><br><p><a href="${imageUrl}">Clique aqui</a></p>`;
+
         return await this.mailService.sendMail({
             to: toEmail,
             from: 'apistagetree@gmail.com',
@@ -33,14 +36,17 @@ export class EmailController {
         });
     }
 
-    @Get('app/user')
-    async sendUserEmail(
-        @Query('toEmail') toEmail,
-        @Query('type') type,
-        @Query('imageUrl') imageUrl,
-    ) {
+    @Post('app/user')
+    async sendUserEmail(@Body() body: any) {
+        const {
+            toEmail,
+            type,
+            imageUrl
+        } = body;
+
         const titleContent = `Sua imagem foi enviada com sucesso!`;
         const htmlContent = `<h2>Imagem escolhida para ${type}</h2>.<br><p><a href="${imageUrl}">Link da imagem</a></p>`;
+
         return await this.mailService.sendMail({
             to: toEmail,
             from: 'apistagetree@gmail.com',
