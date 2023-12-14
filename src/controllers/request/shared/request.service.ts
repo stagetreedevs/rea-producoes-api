e6408bec2870a1f1596e68bbd5af38e327e71326
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-12-14 16:37:59 -0300

    fix: name to firebase file

--- src/controllers/request/shared/request.service.ts
+++ src/controllers/request/shared/request.service.ts
@@ -18,6 +18,19 @@ export class RequestService {
         return uuidv4();
     }
 
+    getCurrentDateWithMinutes(): string {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const hours = String(currentDate.getHours()).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        return formattedDate;
+    }
+
     async list() {
         return await this.reqModel.find().exec();
     }
@@ -81,7 +94,7 @@ export class RequestService {
             const metadata = {
                 contentType: `${type}`,
             };
-            const name = `${this.generateUUID()}.${type}`;
+            const name = `${this.getCurrentDateWithMinutes()}_${this.generateUUID()}.${type}`;
             const fileRef = ref(storage, `requests/${email}/${name}`);
             const uploaded = await uploadBytes(fileRef, file.buffer, metadata);
             const url = await getDownloadURL(uploaded.ref);
