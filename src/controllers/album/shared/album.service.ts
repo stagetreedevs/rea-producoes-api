5d8eed5c8100025d214390e5ee61dc6a7de8ac44
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-12-15 18:29:48 -0300

    fix: name album uploads

--- src/controllers/album/shared/album.service.ts
+++ src/controllers/album/shared/album.service.ts
@@ -7,6 +7,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { FolderService } from 'src/controllers/folder/shared/folder.service';
 import { KeyService } from 'src/controllers/key/shared/key.service';
 import { forkJoin } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class AlbumService {
 
@@ -90,15 +91,35 @@ export class AlbumService {
         }
     }
 
+    generateUUID(): string {
+        return uuidv4();
+    }
+
+    getCurrentDateWithMinutes(): string {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const hours = String(currentDate.getHours()).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+        const milliseconds = String(currentDate.getMilliseconds()).padStart(3, '0');
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+        return formattedDate;
+    }
+
     async upload(path: any, file: Express.Multer.File) {
         const storage = getStorage();
-        const { originalname } = file;
+        // const { originalname } = file;
         const { mimetype } = file;
         const type = mimetype.split('/').join('.');
         const metadata = {
             contentType: `${type}`,
         };
-        const fileRef = ref(storage, `${path.path}/${originalname}`);
+
+        const name = `${this.getCurrentDateWithMinutes()}_${this.generateUUID()}.${type}`
+        const fileRef = ref(storage, `${path.path}/${name}`);
         const uploaded = await uploadBytes(fileRef, file.buffer, metadata);
 
         const link = {
